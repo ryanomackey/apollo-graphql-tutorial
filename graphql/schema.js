ad8a8fe7efbf7fac88ac5c14099103ac22a9e445
Ryan Mackey <ryan.o.mackey@gmail.com>
2017-06-04 15:28:19 -0500

    create the schema file

---
+++ graphql/schema.js
@@ -0,0 +1,21 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+// import resolvers from './resolvers';
+
+const typeDefs = `
+  type Pokemon {
+    id: ID
+    name: String
+  }
+
+  type Query {
+    pokemon(name: String!): Pokemon
+  }
+`;
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  // resolvers,
+});
+
+export default schema;
