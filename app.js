d1c5a520fcbf583e06e0ed5c39fdfbc263fb7c1e
Ryan Mackey <ryan.o.mackey@gmail.com>
2017-06-04 15:20:40 -0500

    create graphql endpoints

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+
+// import schema from './graphql/schema';
+
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'graphql-server-express';
 
 const PORT = 3000;
 const app = express();
 
 app.use('*', cors());
+
+app.post('/graphql', bodyParser.json(), graphqlExpress({
+  // schema,
+}));
+
+app.get('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
